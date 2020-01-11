874fe0cc9d2167f68eb7e857d14e9b0dd0792e31
kaznak <kaznak.at.work@gmail.com>
2020-01-12 02:21:14 +0900

    update test

--- test/chapter1-2/chapter1-2.test.js
+++ test/chapter1-2/chapter1-2.test.js
@@ -28,3 +28,14 @@ test("scalar values", () => {
   expect(chap1.isIrreducableGaussianMatrix(val)).toBe(false);
   expect(chap1.isGaussianMatrix(val)).toBe(false);
 });
+
+test("matrix values", () => {
+  let val;
+
+  val = [
+    [0, 0],
+    [0, 0]
+  ];
+  expect(chap1.isIrreducableGaussianMatrix(val)).toBe(true);
+  expect(chap1.isGaussianMatrix(val)).toBe(true);
+});

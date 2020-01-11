a5a9c309170d5f6201605c6043a48b227c641c20
kaznak <kaznak.at.work@gmail.com>
2020-01-12 02:29:28 +0900

    add chapter 1 section 2 example 6

---
+++ test/chapter1-2/chapter1-2.example6.test.js
@@ -0,0 +1,26 @@
+const math = require("mathjs");
+const chap1 = require("../../src/chapter1-2");
+
+test("example 6", () => {
+  let val = [
+    [1, 3, -2, 0, 2, 0, 0],
+    [2, 6, -5, -2, 4, -3, -1],
+    [0, 0, 5, 10, 0, 15, 5],
+    [2, 6, 0, 8, 4, 18, 6]
+  ];
+  let ret = [
+    [1, 3, 0, 4, 2, 0, 0],
+    [0, 0, 1, 2, 0, 0, 0],
+    [0, 0, 0, 0, 0, 1, "1/3"],
+    [0, 0, 0, 0, 0, 0, 0]
+  ].map(r => r.map(v => math.fraction(v)));
+
+  let out = chap1.gaussJordanElimination(val);
+  expect(chap1.isIrreducableGaussianMatrix(out)).toBe(true);
+  expect(
+    math
+      .equal(out, ret)
+      .flat()
+      .every(v => v)
+  ).toBe(true);
+});

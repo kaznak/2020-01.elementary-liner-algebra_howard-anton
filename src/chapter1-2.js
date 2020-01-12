7fa3ffe7bd03554bab9aa59172861e4bb2f0d077
kaznak <kaznak.at.work@gmail.com>
2020-01-12 12:41:49 +0900

    refactor

--- src/chapter1-2.js
+++ src/chapter1-2.js
@@ -2,15 +2,21 @@ const math = require("mathjs");
 
 function toArray(x) {
   if (x instanceof Array) return x;
-  else if (x instanceof math.Matrix) return x.toArray();
-  else return undefined;
+  if (x instanceof math.Matrix) return x.toArray();
+  throw new TypeError("argument must be a mathjs matrix");
 }
 
 // ガウス行列
 function isGaussianMatrix(x) {
-  const val = toArray(x);
-  if (undefined == val) return false;
-  if (2 != math.size(val).length) return false;
+  let tmp;
+  try {
+    tmp = toArray(x);
+  } catch (err) {
+    return false;
+  }
+  const m = tmp;
+  const [numRow, _, rest] = math.size(m);
+  if (undefined != rest) return false;
 
   // 1. もし、ある行が 0 以外の数を含めば、最初の 0 でない数は 1 である。
   // 2. もし、すべての数が 0 であるような行が含まれていれば、それらの行は下の方に寄せ集められている。
@@ -18,35 +24,39 @@ function isGaussianMatrix(x) {
   let initialOneIndex = -1;
   let notAllZero = true;
 
-  for (ri in val) {
-    const row = val[ri];
-    const vi = row.findIndex(v => math.unequal(0, v));
+  for (let ri = 0; ri < numRow; ri++) {
+    const row = m[ri];
+    const ci = row.findIndex(v => math.unequal(0, v));
 
-    if (-1 == vi) {
+    if (-1 == ci) {
       // cond2
       notAllZero = false;
       continue;
     }
 
-    const v = row[vi];
-
     if (
-      1 == v && // cond1
+      1 == row[ci] && // cond1
       notAllZero && // cond2
-      vi > initialOneIndex // cond3
+      ci > initialOneIndex // cond3
     )
-      initialOneIndex = vi;
+      initialOneIndex = ci;
     else return false;
   }
 
   return true;
 }
 
-// ガウス行列
+// 既約ガウス行列
 function isIrreducableGaussianMatrix(x) {
-  const val = toArray(x);
-  if (undefined == val) return false;
-  if (2 != math.size(val).length) return false;
+  let tmp;
+  try {
+    tmp = toArray(x);
+  } catch (err) {
+    return false;
+  }
+  const m = tmp;
+  const [numRow, _, rest] = math.size(m);
+  if (undefined != rest) return false;
 
   // 1. もし、ある行が 0 以外の数を含めば、最初の 0 でない数は 1 である。
   // 2. もし、すべての数が 0 であるような行が含まれていれば、それらの行は下の方に寄せ集められている。
@@ -54,28 +64,24 @@ function isIrreducableGaussianMatrix(x) {
   // 4. 先頭の1を含む列の他の数はすべて 0 である。
   let initialOneIndex = -1;
   let notAllZero = true;
-  let numRow = val.length;
 
-  for (ri in val) {
-    const row = val[ri];
-    const vi = row.findIndex(v => math.unequal(0, v));
+  for (let ri = 0; ri < numRow; ri++) {
+    const row = m[ri];
+    const ci = row.findIndex(v => math.unequal(0, v));
 
-    if (-1 == vi) {
+    if (-1 == ci) {
       // cond2
       notAllZero = false;
       continue;
     }
 
-    const v = row[vi];
-    const col = math.subset(val, math.index(math.range(0, numRow), vi)).flat();
-
     if (
-      1 == v && // cond1
+      1 == row[ci] && // cond1
       notAllZero && // cond2
-      vi > initialOneIndex && // cond3
-      col.every((v, i) => math.equal(0, v) || i == ri) // cond4
+      ci > initialOneIndex && // cond3
+      m.map(r => r[ci]).every((v, i) => math.equal(0, v) || i == ri) // cond4
     )
-      initialOneIndex = vi;
+      initialOneIndex = ci;
     else return false;
   }
 
@@ -83,14 +89,11 @@ function isIrreducableGaussianMatrix(x) {
 }
 
 function gaussJordanElimination(x) {
-  let m = toArray(x);
-  if (undefined == m) return undefined;
-  const size = math.size(m);
-  if (2 != size.length) return undefined;
+  let m = math.map(toArray(x), v => math.fraction(v));
 
-  const numRow = size[0];
-  const numCol = size[1];
-  m = m.map(r => r.map(v => math.fraction(v)));
+  const [numRow, numCol, rest] = math.size(m);
+  if (undefined != rest)
+    throw new TypeError("argument must be a two dimensional mathjs matrix");
 
   for (let ri = 0, ci = 0; ri < numRow && ci < numCol; ri++) {
     // ステップ1. すべての数が 0 ではない列(縦に並んだ数)のうち最も左の列に注目する。
@@ -112,14 +115,11 @@ function gaussJordanElimination(x) {
     // ステップ4. 第1行に適当な数をかけて、「先頭の1」より下にある0でない数をふくむ行に加え、「先頭の1」より下の数をすべて0にする。
     // ステップ6. 「先頭の1」より上がすべて0になるように、「先頭の1」をふくむ行に適当な数をかけて、それよりも上の行に加える。
     const a = m[ri][ci];
-    const row = m[ri].map(v => v.div(a));
+    const row = math.divide(m[ri], a);
     m = m.map((r, i) => {
       if (ri == i) return row;
       if (math.equal(0, r[ci])) return r;
-      return math.subtract(
-        r,
-        row.map(v => v.mul(r[ci]))
-      );
+      return math.subtract(r, math.multiply(row, r[ci]));
     });
     // ステップ5. 第1行を忘れて、残った行列について、ステップ1にもどる。これを全体がガウス行列になるまで続ける。
   }

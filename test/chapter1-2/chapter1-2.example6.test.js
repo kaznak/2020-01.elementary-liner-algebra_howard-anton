e7bc83a3b2e56807e7fd2a722e0241fc5e8df6ae
kaznak <kaznak.at.work@gmail.com>
2020-01-12 13:37:42 +0900

    refactor test

--- test/chapter1-2/chapter1-2.example6.test.js
+++ test/chapter1-2/chapter1-2.example6.test.js
@@ -9,11 +9,11 @@ test("example 6", () => {
     [2, 6, 0, 8, 4, 18, 6]
   ];
   let ret = [
-      [1, 3, 0, 4, 2, 0, 0],
-      [0, 0, 1, 2, 0, 0, 0],
-      [0, 0, 0, 0, 0, 1, "1/3"],
-      [0, 0, 0, 0, 0, 0, 0]
-    ];
+    [1, 3, 0, 4, 2, 0, 0],
+    [0, 0, 1, 2, 0, 0, 0],
+    [0, 0, 0, 0, 0, 1, "1/3"],
+    [0, 0, 0, 0, 0, 0, 0]
+  ];
 
   let out = chap1.gaussJordanElimination(val);
   expect(chap1.isIrreducableGaussianMatrix(out)).toBe(true);
